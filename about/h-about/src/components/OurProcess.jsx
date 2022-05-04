1a729c81922a1e62a471c210b0ea4f7c0f985867
starydv7 <pawanstar72926@gmail.com>
2022-05-04 18:33:26 +0530

    process

--- about/h-about/src/components/OurProcess.jsx
+++ about/h-about/src/components/OurProcess.jsx
@@ -9,7 +9,7 @@ const OurProcess = () => {
         </div>
         <div className='be2process'>
                 <h1 className='rootprocess'>Purity and Efficacy</h1>
-                  <p>The herbs used in Himalaya Herbal Healthcare's scientific product line are also <br></br>used in the world's oldest traditional system of medicine, Ayurveda, which dates<br></br>
+                  <p className='processp'>The herbs used in Himalaya Herbal Healthcare's scientific product line are also <br></br>used in the world's oldest traditional system of medicine, Ayurveda, which dates<br></br>
                   back over 4,000 years.
                   <br></br>
                   <br></br>
@@ -40,27 +40,38 @@ const OurProcess = () => {
                   knowledge to be used successfully.
                 </p>
           </div>
-          <div className='s2'>
-                <h2>Our Farms</h2>
-                <p className='p'>Founded in 1930, the Himalaya legacy now spans over 90 years, stretching from
+          <div className='s2process'>
+                <h2 className='processh'>Our Farms</h2>
+                <p className='process'>Founded in 1930, the Himalaya legacy now spans over 90 years, stretching from
                     <br></br>one man, to his son, to his grandson. From our humble beginnings in India, our<br></br>
                     products are now offered in over 100 countries, yet we’re still family owned......
                     <br></br>
-                    <br></br>
+                  <br></br>
+                  <br></br>
                     Himalaya offers a full line of clinically-studied herbal formulations, certified USDA <br></br>
                     organic Single herbs, and a full line of body care products. Integrating the principles<br></br>
                     of traditional medicine with modern science, Himalaya is a worldwide pioneer<br></br> in the field of scientifically validated herbal healthcare.
                     <br></br>
-                    <br></br>
+                  <br></br>
+                  <br></br>
                     Himalaya's North American LEED® Gold-Certified eco-friendly corporate office and <br></br>
                     warehouse facility are located in Sugar Land, Texas, a community neighboring <br></br>Houston.
                 </p>
             </div>
-            <div className="img2">
-                <img src='https://i.shgcdn.com/6fdef74e-1e57-4bd4-8f3f-617132184feb/-/format/auto/-/preview/600x700/-/quality/lighter/'alt='him'/>
+            <div className="img2process">
+                <img src='https://i.shgcdn.com/6fdef74e-1e57-4bd4-8f3f-617132184feb/-/format/auto/-/preview/720x700/-/quality/lighter/'alt='him'/>
           </div>
-          <div>
-              <p>asdhawhdashjdsahjsadhgdsaghdsahdsahsadhjsda</p>
+          <div className='farms'>
+              <p>Himalaya chooses farm lands based on soil fertility, suitability and quality, visiting the selected land first to ensure the land is free from soil and water erosion, free from<br></br>
+                  pollution and away from industrial zones, free from municipal waste and the flow of rain water from neighboring fields.
+              <p>Farmers are trained in preparing the land, through to post-harvest management of the herbs, and even in proper drying, packaging, storing and labeling.</p>
+              <p>Himalaya’s farmers use only authenticated starting materials; viable seeds with a good germination percentage. A respective Package of Practices (POP) assures<br></br> adherence to quality, sustainability and careful environmental stewardship at all times.</p>
+              <p>Farmers are trained on earth-friendly organic practices, soil bed preparation, proper irrigation, fertilization and sustainable harvesting, crop rotation, cover crop <br></br>cultivation and organic soil enrichment. No pesticides are allowed to be used.</p>
+              <p>Careful safety measures are put in place for handling threatened plant populations, and farmers are continually trained on the Good Cultivation Practices specific to<br></br> each herb. In addition, farmers are trained on Good Agriculture Collection Practices as well.</p>
+              <p>In addition to initial training, two residential workshops are conducted per year, and supervisors visit the farmer’s field regularly to check for compliance in areas such as<br></br> proper crop management, no chemical usage, no child labor, and appropriate labor hours for adults. Violations do not occur.</p>
+            <p>Himalaya’s agricultural supervisory teams visit farms quarterly, but the frequency of visits may increase depending on the individual needs of the farmer. Himalaya’s <br></br>inspectors and trainers include our own people with Masters Degrees in botany, MBAs in agri-business management, along with agricultural scientists from reputed <br></br>universities</p>
+              </p>
+              
           </div>
 
 
@@ -70,9 +81,9 @@ const OurProcess = () => {
         <img src='https://i.shgcdn.com/1ccc967d-5c31-4ca5-8ceb-3a0c95177b71/-/format/auto/-/preview/600x800/-/quality/lighter/'alt="story"/>
 
         </div>
-        <div className='be2'>
-                <h1 className='root'>Our Manufacturing</h1>
-                  <p>The herbs used in Himalaya Herbal Healthcare's scientific product line are also <br></br>used in the world's oldest traditional system of medicine, Ayurveda, which dates<br></br>
+        <div className='be2m'>
+                <h1 className='rootmanufacture'>Our Manufacturing</h1>
+                  <p className='para'>The herbs used in Himalaya Herbal Healthcare's scientific product line are also <br></br>used in the world's oldest traditional system of medicine, Ayurveda, which dates<br></br>
                   back over 4,000 years.
                   <br></br>
                   <br></br>
